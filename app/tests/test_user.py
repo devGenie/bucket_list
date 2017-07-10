1a03702ba5d80e45e9d68c1e297a2ab764ef8065
devgenie <jonen54@gmail.com>
2017-07-10 17:47:50 +0300

    Created test for user functionality

---
+++ app/tests/test_user.py
@@ -0,0 +1,18 @@
+import unittest
+from models.user import User
+
+class TestUser(unittest.TestCase):
+	def test_user_created(self):
+		pass
+
+	def test_add_bucketlist(self):
+		pass
+
+	def test_view_bucketlist(self):
+		pass
+
+	def test_edit_bucket_list(self):
+		pass
+
+	def test_delete_bucket_list(self):
+		pass
