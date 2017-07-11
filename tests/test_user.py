cfe61def0a37aca6d1941fcbae390a88487d1c3d
devgenie <jonen54@gmail.com>
2017-07-12 00:36:00 +0300

    Added empty test files

---
+++ tests/test_user.py
@@ -0,0 +1,19 @@
+import unittest
+import context
+from app.models import user
+
+class TestUser(unittest.TestCase):
+	def test_user_created(self):
+		pass
+
+	def test_add_bucketlist(self):
+		pass
+
+	def test_view_bucketlist(self):
+		pass
+
+	def test_edit_bucket_list(self):
+		pass
+
+	def test_delete_bucket_list(self):
+		pass
