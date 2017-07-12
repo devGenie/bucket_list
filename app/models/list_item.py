5cdbdf364c36b69e80be7266862b12c462aea73c
devgenie <jonen54@gmail.com>
2017-07-12 17:49:00 +0300

    Implemented functionality for bucketlist item

--- app/models/list_item.py
+++ app/models/list_item.py
@@ -1,6 +1,13 @@
-class ListItem(self):
-	def __init__(self):
-		pass
+import time
+class ListItem(object):
+	def __init__(self,name):
+		self.name=name
+		self.complete_status=False
+		self.date_completed=""
 
 	def show_info(self):
-		pass
+		return {"name":self.name,"complete_status":self.complete_status}
+
+	def mark_complete(self):
+		self.complete_status=True
+		self.time_completed=time.strftime("%d/%m/%Y")+" "+time.strftime("%I:%M:%S")
