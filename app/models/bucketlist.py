ebd62dba7d8fe53649a6402ac731fe04f438e65a
devgenie <jonen54@gmail.com>
2017-07-12 05:46:44 +0300

    Created methods to implement functionality

---
+++ app/models/bucketlist.py
@@ -0,0 +1,21 @@
+class BucketList(object):
+	def __init__(self,name,due_age):
+		pass
+
+	def get_info(self):
+		pass
+
+	def view_items(self):
+		pass
+
+	def add_item(self,name):
+		pass
+
+	def edit_item(self,item,name):
+		pass
+
+	def delete_item(self,name):
+		pass
+
+	def complete_item(self,name):
+		pass
