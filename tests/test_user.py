2df48a49eca5f6e1e84d64107271eb3338f2572b
devgenie <jonen54@gmail.com>
2017-07-12 05:22:27 +0300

    Added tests for user functionality

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,17 +3,31 @@ import context
 from app.models import user
 
 class TestUser(unittest.TestCase):
+	def setUp(self):
+		self.test_user=user.User("Onen","Julius")
 	def test_user_created(self):
-		pass
+		self.assertEqual(self.test_user.name,"Onen Julius","User name not initialized")
 
 	def test_add_bucketlist(self):
-		pass
+		bucket_lists=self.test_user.view_bucketlist()
+		self.test_user.add_bucketlist("before 30",30)
+		new_bucket_lists=self.test_user.view_bucketlist()
+		self.assertNotEqual(len(bucket_lists),len(new_bucket_lists),"Bucketlist not added")
 
 	def test_view_bucketlist(self):
-		pass
+		self.test_user.add_bucketlist("before 30",30)
+		bucket_list=self.test_user.view_bucketlist("before 30")
+		self.assertDictEqual({"name":"before 39","due_age":30},bucket_list,"Bucket list not returned")
 
 	def test_edit_bucket_list(self):
-		pass
+		self.test_user.add_bucketlist("before 25",25)
+		bucket_list=self.test_user.view_bucketlist("before 25")
+		self.test_user.edit_bucketlist(bucket_list,"before 30",30)
+		bucket_list_edit=self.test_user.view_bucketlist
 
 	def test_delete_bucket_list(self):
-		pass
+		self.test_user.add_bucketlist("My bucketist",40)
+		bucket_lists1=self.test_user.view_bucketlist("My bucketist")
+		self.test_user.delete(bucket_list,"My bucketlist",40)
+		bucket_lists2=self.test_user.view_bucketlist()
+		self.assertNotEqual(len(bucket_list),len(bucket_lists1),"Bucket list has not been deleted")
